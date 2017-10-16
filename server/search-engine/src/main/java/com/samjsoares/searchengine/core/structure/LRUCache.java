61707af823b8a1e27693eb9262ca524258febfff
Samuel Soares <samuelj.soares@scranton.edu>
2017-10-15 22:35:00 -0400

    Add default limit and javadoc

--- server/search-engine/src/main/java/com/samjsoares/searchengine/core/structure/LRUCache.java
+++ server/search-engine/src/main/java/com/samjsoares/searchengine/core/structure/LRUCache.java
@@ -3,16 +3,28 @@ package com.samjsoares.searchengine.core.structure;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
-public class LRUCache<K,V> extends LinkedHashMap<K, V> {
-    private final int limit;
+/**
+ * The LRUCache class is a list that removes the least recently used item when the size of the list
+ * reaches the specified limit.
+ * @param <K>
+ * @param <V>
+ */
+public class LRUCache<K, V> extends LinkedHashMap<K, V> {
+  private final static int DEFAULT_LIMIT = 128;
+  private final int limit;
 
-    public LRUCache (int limit) {
-        super(16, 0.75f, true);
-        this.limit = limit;
-    }
+  public LRUCache() {
+    super(16,0.75f, true);
+    this.limit = DEFAULT_LIMIT;
+  }
 
-    @Override
-    protected boolean removeEldestEntry(Map.Entry<K,V> eldest) {
-        return size() > limit;
-    }
+  public LRUCache(int limit) {
+    super(16, 0.75f, true);
+    this.limit = limit;
+  }
+
+  @Override
+  protected boolean removeEldestEntry(Map.Entry<K, V> eldest) {
+    return size() > limit;
+  }
 }
