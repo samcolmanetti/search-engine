ad6f61b808919cd89cdcd97944cd6b8f7ddf7d86
Samuel Soares <samuelj.soares@scranton.edu>
2017-11-28 15:40:21 -0500

    Handle empty results from query

--- website/templates/search.php
+++ website/templates/search.php
@@ -4,15 +4,20 @@
 </form>
 
 <?php
+  $query = $_GET["q"];
   $url = "localhost:8080/api/search?query=" . $_GET["q"];
 
   $results = json_decode(curl($url));
-
-  foreach ($results as $result) {
-    echo '<div class="result">';
-    echo '<a class="title" href="'. $result->url . '">' .$result->title .'</a>';
-    echo '<p class="url">' . $result->url . '</p>';
-    echo '<p class="description">' . $result->description . '</p>';
-    echo '</div>';
+  if (!empty($results)) {
+    foreach ($results as $result) {
+      echo '<div class="result">';
+      echo '<a class="title" href="'. $result->url . '">' .$result->title .'</a>';
+      echo '<p class="url">' . $result->url . '</p>';
+      echo '<p class="description">' . $result->description . '</p>';
+      echo '</div>';
+    }
+  } else {
+    echo "<p> Your search - <strong>{$query}</strong> - did not match any documents.";
   }
+
 ?>
